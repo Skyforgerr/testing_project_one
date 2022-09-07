0e3ec2e7b4f75086cf02878388cfa06333880e64
iking007 <mahinka007@mail.ru>
2022-09-08 00:30:29 +0300

    Приклеил bootstrapfx, дал начало интерфейсу, и обавил файлы по минимуму (констроллер и fxml)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.testing_project_one {
     requires javafx.controls;
     requires javafx.fxml;
+    requires aquafx;
 
 
     opens com.example.testing_project_one to javafx.fxml;
