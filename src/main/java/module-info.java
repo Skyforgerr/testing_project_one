a15ba6c8df4c938a5d55b0a7fdcd9b8bb4f65880
Skyforgerr <postnikoviivan@gmail.com>
2022-09-08 14:45:04 +0300

    new commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module com.example.testing_project_one {
     requires javafx.controls;
     requires javafx.fxml;
-    requires aquafx;
 
 
     opens com.example.testing_project_one to javafx.fxml;
