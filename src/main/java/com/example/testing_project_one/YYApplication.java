65d509d0c1cf84b53084a151a71de24a97deba71
iking007 <mahinka007@mail.ru>
2022-09-09 22:01:31 +0300

    Поправки в файлах, теперь меньше предупреждений, ЕЩЁ МЕНЬШЕ

--- src/main/java/com/example/testing_project_one/YYApplication.java
+++ src/main/java/com/example/testing_project_one/YYApplication.java
@@ -1,22 +1,12 @@
 package com.example.testing_project_one;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.Objects;
 
 
@@ -27,7 +17,7 @@ public class YYApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent content = FXMLLoader.load(getClass().getClassLoader().getResource("YY.fxml"));
+        Parent content = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("YY.fxml")));
         // Устанавливаем минимальный размер окна
         stage.setMinHeight(550);
         stage.setMinWidth(700);
