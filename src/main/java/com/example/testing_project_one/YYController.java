6c6040ec2e1818b74652131592ad1dfa2767059b
iking007 <mahinka007@mail.ru>
2022-09-09 22:04:35 +0300

    Поправки в файлах, теперь меньше предупреждений, ЕЩЁ МЕНЬШЕ ЕЕЕЩЁЁЁЁЁ

--- src/main/java/com/example/testing_project_one/YYController.java
+++ src/main/java/com/example/testing_project_one/YYController.java
@@ -19,7 +19,7 @@ public class YYController {
         Group group = new Group();
         Scene scene = new Scene(group);
         Button button = new Button("Удалить всё");
-        button.setStyle("-fx-background-color: Red; -fx-border-style: 0px,0px,5px,5px; -fx-border-color: darkred; -fx-border-radius: 50px; -fx-padding: 10px");
+        button.setStyle("-fx-background-color: Red; -fx-border-color: darkred; -fx-border-radius: 50px; -fx-padding: 10px");
         button.minHeight(100);
         button.minWidth(100);
 
