33931335319fedd93a0c877668d62628a67396de
iking007 <mahinka007@mail.ru>
2022-09-09 16:51:56 +0300

    Сделан основной интерфейс, адаптивный и с bootstrap

--- src/main/java/com/example/testing_project_one/YYApplication.java
+++ src/main/java/com/example/testing_project_one/YYApplication.java
@@ -11,6 +11,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -26,51 +27,17 @@ public class YYApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Group group = new Group();
-        Parent content = FXMLLoader.load(getClass().getClassLoader().getResource("my.fxml"));
-        Scene scene = new Scene(group, 750, 500);
-        // Подключаем bootstrap
-        scene.getStylesheets().add("org/kordamp/bootstrapfx/bootstrapfx.css");
-        BorderPane borderPane = new BorderPane();
-        borderPane.setMinHeight(500);
-        borderPane.setMinWidth(750);
+        Parent content = FXMLLoader.load(getClass().getClassLoader().getResource("YY.fxml"));
+        // Устанавливаем минимальный размер окна
+        stage.setMinHeight(550);
+        stage.setMinWidth(700);
+        AnchorPane anchorPane = new AnchorPane();
+        Scene scene = new Scene(anchorPane);
+        anchorPane.getChildren().add(content);
 
         stage.setScene(scene);
-        // Создание меню бара
-        MenuBar menuBar = new MenuBar();
-        Menu menu_one = new Menu("Главное");
-        MenuItem item_one = new MenuItem("Настройки");
-        item_one.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                try {
-                    settings(new Stage());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        menu_one.getItems().add(item_one);
-        menuBar.getMenus().add(menu_one);
-        VBox vBox = new VBox();
-        vBox.getChildren().add(menuBar);
-        //borderPane.setTop(vBox);
 
-
-        Button button = new Button("Нажми");
-        button.getStyleClass().add("btn");
-        button.setStyle("");
-        //borderPane.setBottom(button);
-
-        group.getChildren().add(content);
         stage.setTitle("УУ Учёт, Управление");
-        stage.show();
-    }
-    public void settings(Stage stage) throws Exception {
-        Group group_settings = new Group();
-        Scene settings = new Scene(group_settings, 300, 200);
-        stage.setScene(settings);
-        stage.setTitle("Настройки");
         stage.show();
     }
 
