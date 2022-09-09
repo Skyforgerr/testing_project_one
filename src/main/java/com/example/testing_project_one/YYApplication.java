96f16af9936251bd276ca0aa3c97960d744b1c33
iking007 <mahinka007@mail.ru>
2022-09-09 23:09:29 +0300

    Добавлен выход из всего приложения, при закрытии главного окна

--- src/main/java/com/example/testing_project_one/YYApplication.java
+++ src/main/java/com/example/testing_project_one/YYApplication.java
@@ -1,11 +1,14 @@
 package com.example.testing_project_one;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.util.Objects;
 
@@ -29,6 +32,13 @@ public class YYApplication extends Application {
 
         stage.setTitle("УУ Учёт, Управление");
         stage.show();
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
     }
 
     public static void main(String[] args) {
