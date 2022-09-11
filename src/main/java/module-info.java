108fa59654735a60da62dac770e1ccd3644709e7
Skyforgerr <postnikoviivan@gmail.com>
2022-09-12 01:00:39 +0300

    commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.testing_project_one {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.testing_project_one to javafx.fxml;
