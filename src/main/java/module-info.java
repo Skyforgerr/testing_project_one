e53bb21dfba47752f05ed1293ffb0e93e29a6cc3
Skyforgerr <postnikoviivan@gmail.com>
2022-09-11 13:15:07 +0300

    added database and an error window

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.testing_project_one {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
 
 
     opens com.example.testing_project_one to javafx.fxml;
